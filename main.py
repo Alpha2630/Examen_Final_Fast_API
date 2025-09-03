e13a6305731860fee4d22768038b7f9d835f8edc
Alking <alrey2635@gmail.com>
2025-09-03 12:21:08 +0300

    update

--- main.py
+++ main.py
@@ -1,7 +1,40 @@
 from fastapi import FastAPI
+from typing import List
+from fastapi.responses import JSONResponse
 app = FastAPI()
 
 @app.get("/ping")
 def ping():
     return "pong"
+class car(BaseModel):
+    id : str
+    brand: str
+    model: str
+    characteristic: List[max_spid: int, max_fuel_capacity: int]
+automobil: List[car] = []
+@app.post("/cars")
+def cars():
+    return JSONResponse(content= {car}, status_code=201 )
+    
 
+
+
+
+@app.get("/cars")
+def get_cars():
+    return {"cars": [car.dict() for car in automobil]}
+
+
+@app.get("/cars/{id}")
+def update_task(id: int):
+    if 0 <= id < len(automobil):
+        automobil[id] = car
+        return {"cars": [t.dict() for t in automobil]}
+    return JSONResponse(content={"error": "Car non trouvée"}, status_code=404)
+
+
+@app.put("/cars/{id}")
+def update_task(id: int):
+    if 0 <= id < len(automobil):
+        automobil[id] = car
+        return {"cars": [t.dict() for t in automobil]}
